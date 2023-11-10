000f308bac0f078c969087fd75c3723f82542365
fred <fredy.rodriguez@pucp.pe>
2023-11-10 00:14:52 -0500

    tic tac game first wire work in progress and styling finish

--- src/components/GameInfo/GameInfo.jsx
+++ src/components/GameInfo/GameInfo.jsx
@@ -1,6 +1,17 @@
 import styles from "./GameInfo.module.css";
-function GameInfo() {
-  return <div>GameInfo</div>;
+function GameInfo({ moves }) {
+  return (
+    <div className={styles.container}>
+      <button className={styles.reset}>Reset</button>
+
+      <div className={styles.body}>
+        <span>Go to:</span>
+        <div className={styles.grid}>
+          {moves}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default GameInfo;
--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,11 +1,46 @@
 import GameBoard from "../GameBoard";
 import GameInfo from "../GameInfo/GameInfo";
-import styles from "./TicTacToe.module.css"
+import styles from "./TicTacToe.module.css";
+import { useState } from "react";
 function TicTacToe() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    //TODO
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
+  }
+
+  function jumpTo(nextMove) {
+    //TODO
+    setCurrentMove(nextMove)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "move #" + move;
+    } else {
+      description = "game start";
+    }
+    return (
+      <button key={move} onClick={() => jumpTo(move)}>
+        {description}
+      </button>
+    );
+  });
   return (
     <section className={styles.container}>
-      <GameBoard />
-      <GameInfo />
+      <GameBoard
+        xIsNext={xIsNext}
+        squares={currentSquares}
+        onPlay={handlePlay}
+      />
+      <GameInfo moves={moves} />
     </section>
   );
 }

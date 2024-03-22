8a65bc23d02e3f76a69809323e64045e7a9bcd6d
fred <fredy.rodriguez@pucp.pe>
2024-03-22 12:43:20 -0500

    feat: Add states to track currentRow and word sent by user

--- src/components/Wordle-row/Wordle-row.jsx
+++ src/components/Wordle-row/Wordle-row.jsx
@@ -1,13 +1,16 @@
 import s from "./Wordle-row.module.css"
 
-export default function WordleRow() {
+export default function WordleRow({row, word}) {
+  // row = number
+  // word = [... ]
+  let wordLen = word.length
+  let parsedWord = wordLen >= row ? word[row - 1] : '     '
+  console.log(parsedWord)
   return (
     <section className={s["row-container"]}>
-      <span> A</span>
-      <span> A</span>
-      <span> A</span>
-      <span> A</span>
-      <span> A</span>
+      {parsedWord.split('').map(char => {
+        return <span>{char}</span>
+      })}
     </section>
   );
 }
--- src/components/Wordle/Wordle.jsx
+++ src/components/Wordle/Wordle.jsx
@@ -1,25 +1,50 @@
+import { useState } from "react";
 import WordleRow from "../Wordle-row";
 import s from "./Wordle.module.css";
 
 export default function Wordle() {
+  const [currentRow, setCurrentRow] = useState(1);
+  const [word, setWord] = useState([]);
+  function sendWord(event) {
+    if (currentRow > 5) {
+      console.log('You lose')
+      // setCurrentRow(1)
+      setWord([])
+      // reset game ToDo disable Submit
+      return
+    }
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const currentInput = formData.get("word-input");
+    // assign row and word to []
+    let cRow = currentRow;
+    let wordsArray = word;
+    setCurrentRow(cRow + 1);
+    wordsArray.push(currentInput);
+    setWord(wordsArray);
+    console.log(currentInput, currentRow, word);
+    // if currentRow > 5 lose the game
+  }
   return (
     <section className={s.box}>
-      <body className={s.container}>
+      <div className={s.container}>
         <aside>
-          <WordleRow />
-          <WordleRow />
-          <WordleRow />
-          <WordleRow />
-          <WordleRow />
+          <WordleRow row={1} word={word} />
+          <WordleRow row={2} word={word} />
+          <WordleRow row={3} word={word} />
+          <WordleRow row={4} word={word} />
+          <WordleRow row={5} word={word} />
         </aside>
-        <form className={s["form-container"]}>
+        <form onSubmit={sendWord} className={s["form-container"]}>
           <label htmlFor="word-input">Enter your guess</label>
           <div className={s["form-input-section"]}>
             <input type="text" name="word-input" />
-            <button className={s.button}>Submit</button>
+            <button type="submit" className={s.button} disabled={currentRow > 5}>
+              Send
+            </button>
           </div>
         </form>
-      </body>
+      </div>
     </section>
   );
 }

d1a1df1c28c081df49945a68cc8716e67022efa5
fred <fredy.rodriguez@pucp.pe>
2024-03-21 22:53:56 -0500

    feat: Add first layout for wordle, create two react components Wordle and Wordle-row

---
+++ src/components/Wordle-row/Wordle-row.jsx
@@ -0,0 +1,13 @@
+import s from "./Wordle-row.module.css"
+
+export default function WordleRow() {
+  return (
+    <section className={s["row-container"]}>
+      <span> A</span>
+      <span> A</span>
+      <span> A</span>
+      <span> A</span>
+      <span> A</span>
+    </section>
+  );
+}
--- src/components/Wordle/Wordle.jsx
+++ src/components/Wordle/Wordle.jsx
@@ -1,9 +1,25 @@
-import Pending from "../Pending/Pending";
+import WordleRow from "../Wordle-row";
+import s from "./Wordle.module.css";
 
 export default function Wordle() {
   return (
-    <div>
-      <Pending project={"Wordle"}/>
-    </div>
+    <section className={s.box}>
+      <body className={s.container}>
+        <aside>
+          <WordleRow />
+          <WordleRow />
+          <WordleRow />
+          <WordleRow />
+          <WordleRow />
+        </aside>
+        <form className={s["form-container"]}>
+          <label htmlFor="word-input">Enter your guess</label>
+          <div className={s["form-input-section"]}>
+            <input type="text" name="word-input" />
+            <button className={s.button}>Submit</button>
+          </div>
+        </form>
+      </body>
+    </section>
   );
 }
